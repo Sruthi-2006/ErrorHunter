94ac4c6de6f8126f0f799736382eae38247900de
Sruthi <sruthirangaraj3@gmail.com>
2025-07-31 12:10:25 +0530

    removed mixed approaches

--- problems/easy/easy_q8.py
+++ problems/easy/easy_q8.py
@@ -2,14 +2,6 @@
 def reverse_number(num):
  
     rev = 0
-    revs=reversed(num)
-    for x in revs:
-        print(x)
-        digit = num % 10
-        rev+=digit   
- 
- 
-    rev = ""
     while num != 0:
  
         digit = num % 10
@@ -20,15 +12,8 @@ def reverse_number(num):
 if __name__ == "__main__":
     number= int(input("Enter num : "))
     res = reverse_number(number)
- 
-        print(digit)
-        num = num //10  
- 
-        rev = rev*10 + digit   
- 
-        num //= 10
-    return rev   
- 
+    print(res)
+    
  
  
   
